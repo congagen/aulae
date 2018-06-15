62d0469b36c1af9ed22db36725bb9220beaf8a41
Tim Sandgren <tim.sndgrn@gmail.com>
2018-06-15 10:35:34 +0200

    um

--- 0.1/Aumenta/Code/Data/RLM_Feed.swift
+++ 0.1/Aumenta/Code/Data/RLM_Feed.swift
@@ -1,12 +1,24 @@
-import RealmSwift
 import Foundation
+import RealmSwift
+
 
 class RLM_Feed: Object {
-    
-    @objc dynamic var url: String = ""
     
     @objc dynamic var name: String = ""
     @objc dynamic var id: String = ""
     @objc dynamic var info: String = ""
+    @objc dynamic var version: Int = 123456789
+    @objc dynamic var updatedUtx: Int = 0
+    
+    @objc dynamic var errors: Int = 1
+    
+    @objc dynamic var jsonPath: String = ""
+    
+    @objc dynamic var active: Bool = true
+    @objc dynamic var lat: Double = 0.0
+    @objc dynamic var lng: Double = 0.0
+    
+    @objc dynamic var radius: Double = 1000
+    @objc dynamic var url: String = "https://api.myjson.com/bins/h61m6"
     
 }
