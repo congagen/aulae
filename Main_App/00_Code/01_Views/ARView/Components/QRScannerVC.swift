1cda567d569c723b2d02e3938a8518249c43d0af
Tim Sandgren <tim.sndgrn@gmail.com>
2019-11-08 19:12:26 +0100

    Qr view

---
+++ Main_App/00_Code/01_Views/ARView/Components/QRScannerVC.swift
@@ -0,0 +1,30 @@
+//
+//  QRScannerVC.swift
+//  Aulae
+//
+//  Created by Tim Sandgren on 2019-11-08.
+//  Copyright © 2019 Abstraqata. All rights reserved.
+//
+
+import UIKit
+
+class QRScannerVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
