a92a10f7e555976721f5d682703557b10c010c12
Tim Sandgren <tim.sndgrn@gmail.com>
2019-11-12 11:28:40 +0100

    qr fix

--- Main_App/00_Code/01_Views/ARView/Components/QRScannerVC.swift
+++ Main_App/00_Code/01_Views/ARView/Components/QRScannerVC.swift
@@ -6,25 +6,164 @@
 //  Copyright © 2019 Abstraqata. All rights reserved.
 //
 
+import Foundation
+import Vision
+import AVFoundation
 import UIKit
+import ARKit
 
-class QRScannerVC: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+class QRScannerVC: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
 
-        // Do any additional setup after loading the view.
+    //var isTrackingQR = false
+    var qrUrl = ""
+    
+    var qrCapturePreviewLayer: AVCaptureVideoPreviewLayer? = nil
+    var qrCaptureSession: AVCaptureSession? = nil
+    
+    @IBAction func closeBtnAction(_ sender: UIBarButtonItem) {
+    
+        DispatchQueue.global(qos: .default).async {
+            [weak self] in
+
+            DispatchQueue.main.sync {
+                if ((self!.qrCaptureSession) != nil){
+                    self?.qrCaptureSession!.stopRunning()
+                }
+            }
+
+            DispatchQueue.main.async {
+                if ((self!.qrCapturePreviewLayer) != nil) {
+                    self?.qrCapturePreviewLayer?.removeFromSuperlayer()
+                    self?.qrCapturePreviewLayer = nil
+                }
+            }
+        }
+
+        qrCaptureSession?.stopRunning()
+        qrCapturePreviewLayer?.removeFromSuperlayer()
+        //qrCapturePreviewLayer = nil
+        
+        UIOps().updateNavUiMode(navCtrl: self.navigationController!)
+        self.navigationController?.dismiss( animated: true, completion: { super.viewDidAppear(true)} )
+        self.view.removeFromSuperview()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func handleEnterURL(alertView: UIAlertAction!) {
+        print("Adding QRURL: " + qrUrl)
+        FeedActions().addNewSource(feedUrl: qrUrl, feedApiKwd: "", refreshExisting: true)
     }
-    */
+    
+    
+    func openQRUrl(scheme: String) {
+        if let url = URL(string: scheme) {
+            if #available(iOS 10, *) {
+                UIApplication.shared.open(
+                    url, options: [:], completionHandler: { (success) in print("Open \(scheme): \(success)")
+                })
+            } else {
+                let success = UIApplication.shared.openURL(url)
+                print("Open \(scheme): \(success)")
+            }
+        }
+    }
+    
+    
+    func showQRURLAlert(aMessage: String) {
+        let alert = UIAlertController(
+            title: aMessage,
+            message: nil,
+            preferredStyle: UIAlertController.Style.alert
+        )
+        
+        qrUrl = aMessage
+        
+        alert.addAction(UIAlertAction(title: "Cancel",     style: UIAlertAction.Style.cancel,  handler: nil))
+        alert.addAction(UIAlertAction(title: "Add to Lib", style: UIAlertAction.Style.default, handler: handleEnterURL))
+        alert.addAction(UIAlertAction(title: "Open Link",  style: UIAlertAction.Style.default, handler: {_ in self.openQRUrl(scheme: self.qrUrl)} ))
+        
+        alert.view.tintColor = UIColor.black
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        for item in metadataObjects {
+            if let metadataObject = item as? AVMetadataMachineReadableCodeObject {
+                
+                if metadataObject.type == AVMetadataObject.ObjectType.qr {
+                    qrUrl = metadataObject.stringValue!
+                    
+                    if (qrUrl != "") {
+                        print(metadataObject.stringValue!)
+                        showQRURLAlert(aMessage: metadataObject.stringValue!)
+                    }
+                }
+                
+                if metadataObject.type == AVMetadataObject.ObjectType.upce {
+                    print(AVMetadataObject.ObjectType.upce)
+                }
+            }
+        }
+    }
+    
+    
+    func captureQRCode() {
+        print("captureQRCode")
+        view.backgroundColor = UIColor.black
+        qrCaptureSession = AVCaptureSession()
+
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
+        let videoInput: AVCaptureDeviceInput
+
+        do {
+            videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
+        } catch {
+            return
+        }
+
+        if (qrCaptureSession!.canAddInput(videoInput)) {
+            qrCaptureSession!.addInput(videoInput)
+        } else {
+            failed()
+            return
+        }
+
+        let metadataOutput = AVCaptureMetadataOutput()
+
+        if (qrCaptureSession!.canAddOutput(metadataOutput)) {
+            qrCaptureSession!.addOutput(metadataOutput)
+
+            metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            metadataOutput.metadataObjectTypes = [.qr]
+        } else {
+            failed()
+            return
+        }
+
+        qrCapturePreviewLayer = AVCaptureVideoPreviewLayer(session: qrCaptureSession!)
+        qrCapturePreviewLayer!.frame = view.layer.bounds
+        qrCapturePreviewLayer!.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(qrCapturePreviewLayer!)
+
+        qrCaptureSession!.startRunning()
+    }    
+    
+    
+    func failed() {
+        let ac = UIAlertController(title: "Scanning not supported", message: "Your device does not support scanning a code from an item. Please use a device with a camera.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+        qrCaptureSession = nil
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        captureQRCode()
+    }
+    
 
 }
